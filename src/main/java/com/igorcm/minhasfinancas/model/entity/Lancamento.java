ebeb45581ce20516ffaf1fd97a9c328c81640c22
Igor Corona de Matos <32557238+igorcm3@users.noreply.github.com>
2022-01-06 15:31:18 -0300

    refactor: Add lombok, remove boilerplate from models

--- src/main/java/com/igorcm/minhasfinancas/model/entity/Lancamento.java
+++ src/main/java/com/igorcm/minhasfinancas/model/entity/Lancamento.java
@@ -17,6 +17,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.igorcm.minhasfinancas.model.enums.StatusLancamento;
+import com.igorcm.minhasfinancas.model.enums.TipoLancamento;
+
+import lombok.Builder;
+import lombok.Data;
+
 /**
  * @author <a href="coronaigor@gmail.com">Igor Corona de Matos</a>
  *
@@ -24,6 +30,8 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "lancamento", schema = "financas")
+@Data
+@Builder
 public class Lancamento {
 
 	@Id
@@ -57,151 +65,5 @@ public class Lancamento {
 	@ManyToOne
 	@JoinColumn(name = "id_usuario")
 	private Usuario usuario;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Integer getMes() {
-		return mes;
-	}
-
-	public void setMes(Integer mes) {
-		this.mes = mes;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public BigDecimal getValor() {
-		return valor;
-	}
-
-	public void setValor(BigDecimal valor) {
-		this.valor = valor;
-	}
-
-	public LocalDate getDataCadastro() {
-		return dataCadastro;
-	}
-
-	public void setDataCadastro(LocalDate dataCadastro) {
-		this.dataCadastro = dataCadastro;
-	}
-
-	public TipoLancamento getTipoLancamento() {
-		return tipoLancamento;
-	}
-
-	public void setTipoLancamento(TipoLancamento tipoLancamento) {
-		this.tipoLancamento = tipoLancamento;
-	}
-
-	public StatusLancamento getStatusLancamento() {
-		return statusLancamento;
-	}
-
-	public void setStatusLancamento(StatusLancamento statusLancamento) {
-		this.statusLancamento = statusLancamento;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((ano == null) ? 0 : ano.hashCode());
-		result = prime * result + ((dataCadastro == null) ? 0 : dataCadastro.hashCode());
-		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((mes == null) ? 0 : mes.hashCode());
-		result = prime * result + ((statusLancamento == null) ? 0 : statusLancamento.hashCode());
-		result = prime * result + ((tipoLancamento == null) ? 0 : tipoLancamento.hashCode());
-		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
-		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Lancamento other = (Lancamento) obj;
-		if (ano == null) {
-			if (other.ano != null)
-				return false;
-		} else if (!ano.equals(other.ano))
-			return false;
-		if (dataCadastro == null) {
-			if (other.dataCadastro != null)
-				return false;
-		} else if (!dataCadastro.equals(other.dataCadastro))
-			return false;
-		if (descricao == null) {
-			if (other.descricao != null)
-				return false;
-		} else if (!descricao.equals(other.descricao))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (mes == null) {
-			if (other.mes != null)
-				return false;
-		} else if (!mes.equals(other.mes))
-			return false;
-		if (statusLancamento != other.statusLancamento)
-			return false;
-		if (tipoLancamento != other.tipoLancamento)
-			return false;
-		if (usuario == null) {
-			if (other.usuario != null)
-				return false;
-		} else if (!usuario.equals(other.usuario))
-			return false;
-		if (valor == null) {
-			if (other.valor != null)
-				return false;
-		} else if (!valor.equals(other.valor))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Lancamento [id=" + id + ", mes=" + mes + ", ano=" + ano + ", valor=" + valor + ", dataCadastro="
-				+ dataCadastro + ", tipoLancamento=" + tipoLancamento + ", statusLancamento=" + statusLancamento
-				+ ", descricao=" + descricao + ", usuario=" + usuario + "]";
-	}
 
 }
